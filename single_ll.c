2aaca9ae5bb000d16e10d8cafa21235b5ad3227c
Sanjib Kumar Bisoyi <74747542+sanjib-787@users.noreply.github.com>
2026-01-13 06:53:35 +0530

    Refactor linked list implementation and functions
    Refactor linked list functions for improved readability and consistency. Update function signatures and enhance user prompts.

--- single_ll.c
+++ single_ll.c
@@ -1,345 +1,188 @@
-#include"../hdr/header.h"
-struct Node{
-	int data;
-	struct Node *next;
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
 };
-int print_ll( struct Node *temp )
-{
-	while( temp != NULL ){
-		printf("%d ",temp -> data);
-		temp = temp -> next;
-	}
-	printf("\n");	
+
+/* ---------- Print Linked List ---------- */
+void print_ll(struct Node *head) {
+    struct Node *temp = head;
+    if (temp == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
 }
-struct Node *create_ll(struct Node *head, struct Node *temp, int size)
-{
-	struct Node *new_node = NULL;
-	for( int i = 0; i < size; i++){
-		new_node = ( struct Node * ) malloc ( sizeof(struct Node));
-		printf("enter data for ll:\n");
-		scanf("%d",&new_node -> data );
-		new_node -> next = NULL;
-		if( head == NULL ){
-			head = new_node;
-			temp = new_node;
-		} else {
-			temp -> next = new_node;
-			temp = new_node;
-		}
-	}
-	return head;
-}
-struct Node *insert_begning( struct Node *head )
-{
-	struct Node *new_node = NULL;
-	struct Node *temp = NULL;
-	new_node = ( struct Node *) malloc ( sizeof( struct Node * ) );
-	printf("enter value for storing at begning :");
-	scanf( "%d", &new_node -> data );
-	temp = head;
-	head = new_node;
-	head -> next = temp;
+
+/* ---------- Create Linked List ---------- */
+struct Node *create_ll(int size) {
+    struct Node *head = NULL, *temp = NULL, *new_node = NULL;
+
+    for (int i = 0; i < size; i++) {
+        new_node = (struct Node *)malloc(sizeof(struct Node));
+        printf("Enter data: ");
+        scanf("%d", &new_node->data);
+        new_node->next = NULL;
+
+        if (head == NULL) {
+            head = new_node;
+            temp = new_node;
+        } else {
+            temp->next = new_node;
+            temp = new_node;
+        }
+    }
     return head;
 }
-struct Node *insert_end( struct Node *head )
-{
-	struct Node *new_node = NULL;
-	struct Node *temp = NULL;
-	new_node = ( struct Node *) malloc ( sizeof( struct Node * ) );
-	printf("enter value for storing at end :");
-	scanf( "%d", &new_node -> data );
-	new_node -> next = NULL;
-	temp = head;
-	while ( temp -> next != NULL ){
-		temp = temp -> next;
-	}
-	temp -> next = new_node;
-	return head;
+
+/* ---------- Insert at Beginning ---------- */
+struct Node *insert_beginning(struct Node *head, int *size) {
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    printf("Enter data: ");
+    scanf("%d", &new_node->data);
+
+    new_node->next = head;
+    head = new_node;
+    (*size)++;
+    return head;
 }
-struct Node *insert_position(struct Node *head, int* newsize)
-{
-	int i=1,pos, count = 0;
-	struct Node *new_node = NULL;
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	printf("enter position:\n");
-	scanf("%d",&pos);
-	new_node = ( struct Node *) malloc ( sizeof( struct Node * ) );
-	printf("enter value for storing at end :");
-	scanf( "%d", &new_node -> data );
 
-	if (pos > *newsize + 1 || pos <= 0) {
-		printf( "position is greater than no of nodes.\n");
-		return head;
-	} else if(pos == 1) {
-		temp = head;
-		head = new_node;
-		head -> next = temp;
+/* ---------- Insert at End ---------- */
+struct Node *insert_end(struct Node *head, int *size) {
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    printf("Enter data: ");
+    scanf("%d", &new_node->data);
+    new_node->next = NULL;
 
-	} else {
-		temp = head;
-		temp1 = head;
-		while( i < pos ){
-			temp1 = temp;
-			temp = temp -> next;
-			i++;
-			}
-		temp1 -> next = new_node;
-		new_node -> next = temp;
-		*newsize++;
-		}
-	return head;
+    if (head == NULL) {
+        head = new_node;
+    } else {
+        struct Node *temp = head;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = new_node;
+    }
+    (*size)++;
+    return head;
 }
-struct Node *insert_before_position(struct Node *head, int *newsize)
-{
-	int i=1,pos, count = 0;
-	struct Node *new_node = NULL;
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	printf("enter position:\n");
-	scanf("%d",&pos);
-	new_node = ( struct Node *) malloc ( sizeof( struct Node * ) );
-	printf("enter value for storing at end :");
-	scanf( "%d", &new_node -> data );
 
-	if (pos > *newsize + 1 || pos <= 0) {
-		printf( "position is greater than no of nodes.\n");
-		return head;
-	} else if(pos == 1) {
-		temp = head;
-		head = new_node;
-		head -> next = temp;
+/* ---------- Insert at Position ---------- */
+struct Node *insert_at_position(struct Node *head, int *size) {
+    int pos;
+    printf("Enter position (1 to %d): ", *size + 1);
+    scanf("%d", &pos);
 
-	} else {
-	temp = head;
-	temp1 = head;
-	while( i < pos-1 ){
-		temp1 = temp;
-		temp = temp -> next;
-		i++;
-	}
-	temp1 -> next = new_node;
-	new_node -> next = temp;
-	*newsize++;
-	}
-	return head;
+    if (pos < 1 || pos > *size + 1) {
+        printf("Invalid position\n");
+        return head;
+    }
 
+    if (pos == 1)
+        return insert_beginning(head, size);
+
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    printf("Enter data: ");
+    scanf("%d", &new_node->data);
+
+    struct Node *temp = head;
+    for (int i = 1; i < pos - 1; i++)
+        temp = temp->next;
+
+    new_node->next = temp->next;
+    temp->next = new_node;
+    (*size)++;
+    return head;
 }
-struct Node *insert_after_position(struct Node *head, int *newsize)
-{
-	int i=1,pos, count = 0;
-	struct Node *new_node = NULL;
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	printf("enter position:\n");
-	scanf("%d",&pos);
-	new_node = ( struct Node *) malloc ( sizeof( struct Node * ) );
-	printf("enter value for storing at end :");
-	scanf( "%d", &new_node -> data );
 
-	if (pos > *newsize + 1 || pos <= 0) {
-		printf( "position is greater than no of nodes.\n");
-		return head;
-	} else if(pos == 1) {
-		temp = head;
-		head = new_node;
-		head -> next = temp;
+/* ---------- Insert Before a Number ---------- */
+struct Node *insert_before_number(struct Node *head, int *size) {
+    int num;
+    printf("Enter number before which to insert: ");
+    scanf("%d", &num);
 
-	} else {
-	temp = head;
-	temp1 = head;
-	while( i < pos + 1 ){
-		temp1 = temp;
-		temp = temp -> next;
-		i++;
-	}
-	temp1 -> next = new_node;
-	new_node -> next = temp;
-	*newsize++;
-	}
-	return head;
+    if (head == NULL) {
+        printf("List empty\n");
+        return head;
+    }
+
+    if (head->data == num)
+        return insert_beginning(head, size);
+
+    struct Node *temp = head;
+    while (temp->next != NULL && temp->next->data != num)
+        temp = temp->next;
+
+    if (temp->next == NULL) {
+        printf("Number not found\n");
+        return head;
+    }
+
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    printf("Enter data: ");
+    scanf("%d", &new_node->data);
+
+    new_node->next = temp->next;
+    temp->next = new_node;
+    (*size)++;
+    return head;
 }
-struct Node *insert_before_number( struct Node *head ,int *newsize)
-{
-	int i=1, num, count = 0;
-	struct Node *new_node = NULL;
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	printf("enter number:\n");
-	scanf("%d",&num);
-	new_node = ( struct Node *) malloc ( sizeof( struct Node * ) );
-	printf("enter value for storing before number :");
-	scanf( "%d", &new_node -> data );
-	temp = head;
-	temp1 = head;
-	while ( temp -> next != NULL ){
-		temp1 = temp;
-		temp = temp -> next;
-//		i++;
-		if( num == temp -> data){
-//			printf("i is %d\n",i);
-			temp1 -> next = new_node;
-			new_node -> next = temp;
-		} else {
-			printf("number is not present");
-		}
-	}
 
-	return head;
+/* ---------- Insert After a Number ---------- */
+struct Node *insert_after_number(struct Node *head, int *size) {
+    int num;
+    printf("Enter number after which to insert: ");
+    scanf("%d", &num);
+
+    struct Node *temp = head;
+    while (temp != NULL && temp->data != num)
+        temp = temp->next;
+
+    if (temp == NULL) {
+        printf("Number not found\n");
+        return head;
+    }
+
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    printf("Enter data: ");
+    scanf("%d", &new_node->data);
+
+    new_node->next = temp->next;
+    temp->next = new_node;
+    (*size)++;
+    return head;
 }
-struct Node *insert_after_number( struct Node *head ,int *newsize)
-{
-	int i=1,pos, num, count = 0;
-	struct Node *new_node = NULL;
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	printf("enter number:\n");
-	scanf("%d",&num);
-	new_node = ( struct Node *) malloc ( sizeof( struct Node * ) );
-	printf("enter value for storing before number :");
-	scanf( "%d", &new_node -> data );
-	temp = head;
-	temp1 = head;
-	while ( temp -> next != NULL ){
-		temp1 = temp;
-		temp = temp -> next;
-		if( num == temp -> data){
-			break;
-//			new_node -> next = temp1;
-//			temp -> next = new_node;
-		}
-		i++;
-	}
-	temp = head;
-	temp1 = head;
-	while( i < pos + 1 ){
-		temp1 = temp;
-		temp = temp -> next;
-		i++;
-	}
-	temp1 -> next = new_node;
-	new_node -> next = temp;
-	*newsize++;
-	return head;
-}
-struct Node *insert_mid_position(struct Node *head, int* newsize)
-{
-	int i=1,pos, count = 0;
-	struct Node *new_node = NULL;
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-//	printf("enter position:\n");
-//	scanf("%d",&pos);
-	pos = (*newsize) / 2;
-	new_node = ( struct Node *) malloc ( sizeof( struct Node * ) );
-	printf("enter value for storing at end :");
-	scanf( "%d", &new_node -> data );
 
-	if (pos > *newsize + 1 || pos <= 0) {
-		printf( "position is greater than no of nodes.\n");
-		return head;
-	} else if(pos == 1) {
-		temp = head;
-		head = new_node;
-		head -> next = temp;
+/* ---------- MAIN ---------- */
+int main() {
+    int size, choice;
+    struct Node *head = NULL;
 
-	} else {
-	temp = head;
-	temp1 = head;
-	while( i < pos + 1 ){
-		temp1 = temp;
-		temp = temp -> next;
-		i++;
-	}
-	temp1 -> next = new_node;
-	new_node -> next = temp;
-	*newsize++;
-	}
-	return head;
-}
-struct Node *insert_penultimate_position(struct Node *head, int* newsize)
-{
-	int i=1,pos, count = 0;
-	struct Node *new_node = NULL;
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	//printf("enter position:\n");
-	//scanf("%d",&pos);
-	pos = (*newsize) - 1;
-	new_node = ( struct Node *) malloc ( sizeof( struct Node * ) );
-	printf("enter value for storing at end :");
-	scanf( "%d", &new_node -> data );
+    printf("Enter initial size: ");
+    scanf("%d", &size);
 
-	if (pos > *newsize + 1 || pos <= 0) {
-		printf( "position is greater than no of nodes.\n");
-		return head;
-	} else if(pos == 1) {
-		temp = head;
-		head = new_node;
-		head -> next = temp;
+    head = create_ll(size);
 
-	} else {
-	temp = head;
-	temp1 = head;
-	while( i < pos + 1 ){
-		temp1 = temp;
-		temp = temp -> next;
-		i++;
-	}
-	temp1 -> next = new_node;
-	new_node -> next = temp;
-	*newsize++;
-	}
-	return head;
-}
-int main(void)
-{
-	int size, choice;
-	struct Node *head = NULL;
-	struct Node *temp = NULL;
-	struct Node *new_node = NULL;
-	printf("enter size:\n");
-	scanf("%d",&size);
-	head = create_ll( head, temp, size);
-	while(1){
-		printf("1. Insert at the beginning\n2. Insert at the end\n3. Insert at a given position\n4. Insert before a given position\n5. Insert after a given position\n6. Insert before a given number\n7. Insert after a given number\n8. Insert at the middle\n9. Insert at penultimate node\n10. print\n");
-		printf("enter ur choice:\n");
-		scanf("%d",&choice);
-		switch(choice){
-			case 1:
-				head = insert_begning( head );
-				break;
-			case 2:
-				head = insert_end( head );
-				break;
-			case 3:
-				head = insert_position( head , &size);
-				break;
-			case 4:
-				head = insert_before_position( head, &size);
-				break;
-			case 5:
-				head = insert_after_position( head, &size);
-				break;
-			case 6:
-				head = insert_before_number( head, &size);
-				break;
-			case 7:
-				head = insert_after_number( head, &size);
-				break;
-			case 8:
-				head = insert_mid_position( head, &size);
-				break;
-			case 9:
-				head = insert_penultimate_position( head, &size);
-				break;
-			case 10:
-				print_ll( head );
-				break;
-				//exit(EXIT_SUCCESS);
-			default :
-				printf("wrong choice\n");
-				exit(EXIT_SUCCESS);
-		}
-	}
+    while (1) {
+        printf("\n1.Insert Beginning\n2.Insert End\n3.Insert at Position\n");
+        printf("4.Insert Before Number\n5.Insert After Number\n6.Print\n0.Exit\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: head = insert_beginning(head, &size); break;
+            case 2: head = insert_end(head, &size); break;
+            case 3: head = insert_at_position(head, &size); break;
+            case 4: head = insert_before_number(head, &size); break;
+            case 5: head = insert_after_number(head, &size); break;
+            case 6: print_ll(head); break;
+            case 0: exit(0);
+            default: printf("Invalid choice\n");
+        }
+    }
 }
