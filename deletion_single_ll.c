bc674950a47440c9ac28f93077ba75f5a8d70f93
Sanjib Kumar Bisoyi <74747542+sanjib-787@users.noreply.github.com>
2026-01-16 06:57:26 +0530

    Refactor linked list deletion functions and print

--- deletion_single_ll.c
+++ deletion_single_ll.c
@@ -1,300 +1,175 @@
-#include"../hdr/header.h"
-struct Node{
-	int data;
-	struct Node *next;
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
 };
-int print_ll( struct Node *temp )
-{
-	while( temp != NULL ){
-		printf("%d ",temp -> data);
-		temp = temp -> next;
-	}
-	
-}
-struct Node *create_ll(struct Node *head, struct Node *temp, int size)
-{
-	struct Node *new_node = NULL;
-	for( int i = 0; i < size; i++){
-		new_node = ( struct Node * ) malloc ( sizeof(struct Node *));
-		printf("enter data for ll:\n");
-		scanf("%d",&new_node -> data );
-		new_node -> next = NULL;
-		if( head == NULL ){
-			head = new_node;
-			temp = new_node;
-		} else {
-			temp -> next = new_node;
-			temp = new_node;
-		}
-	}
-	temp = head;
-	return temp;
-}
-struct Node *delete_begning(struct Node *head )
-{
-	if ( head == NULL ){
-		printf("list is empty\n");
-	} else {
-		struct Node *temp = NULL;
-		temp = head;
-		head = head -> next;
-		free( temp );
-	}
-	return head;
-}
-struct Node *delete_end(struct Node *head )
-{
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	if ( head == NULL ){
-		printf("list is empty\n");
-	} else {
-		temp = head;
-		temp1 = head;
-		while( temp -> next != NULL){
-			temp1 = temp;
-			temp = temp -> next;
-		}
-			temp1 -> next = NULL;
-			free( temp );
-	}
-	return head;
-}
-struct Node *delete_position( struct Node *head)
-{
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	int i = 1, pos;
-	temp = head;
-	if ( head == NULL ){
-		printf("list is empty\n");
-	} else {
-		printf("Enter the position \n");
-		scanf("%d", &pos);
-		while( i < pos - 1 ) {
-			temp = temp -> next;
-			i++;
-		}
-		temp1 = temp -> next;
-		temp -> next = temp1 -> next;
-		free( temp1 );
-	}
-	return head;
-}
-struct Node *delete_before_position( struct Node *head)
-{
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	int i = 1, pos;
-	temp = head;
-	if ( head == NULL ){
-		printf("list is empty\n");
-	} else {
-		printf("Enter the position \n");
-		scanf("%d", &pos);
-		while( i < pos - 2 ) {
-			temp = temp -> next;
-			i++;
-		}
-		temp1 = temp -> next;
-		temp -> next = temp1 -> next;
-		free( temp1 );
-	}
-	return head;
-}
-struct Node *delete_after_position( struct Node *head)
-{
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	int i = 1, pos;
-	temp = head;
-	if ( head == NULL ){
-		printf("list is empty\n");
-	} else {
-		printf("Enter the position \n");
-		scanf("%d", &pos);
-		while( i < pos ) {
-			temp = temp -> next;
-			i++;
-		}
-		temp1 = temp -> next;
-		temp -> next = temp1 -> next;
-		free( temp1 );
-	}
-	return head;
+
+/* ---------- Print ---------- */
+void print_ll(struct Node *head) {
+    if (!head) {
+        printf("List empty\n");
+        return;
+    }
+    while (head) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
 }
 
-struct Node *delete_before_number( struct Node *head)
-{
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	int i = 1, pos, num;
-	printf("enter a number:\n");
-	scanf("%d", &num);
-	temp = head;
-	while( temp -> next != NULL) {
-		temp = temp -> next;
-		if( num == temp -> data){
-			printf("%d\n", i);
-			pos = i;
-			break;
-		}
-		i++;
-	}
-	temp = head;
-	if ( head == NULL ){
-		printf("list is empty\n");
-	} else {
-		while( i < pos - 1 ) {
-			temp = temp -> next;
-			i++;
-		}
-		temp1 = temp -> next;
-		temp -> next = temp1 -> next;
-		free( temp1 );
-	}
-	return head;
-}
-struct Node *delete_after_number( struct Node *head)
-{
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	int i = 1, pos, num;
-	printf("enter a number:\n");
-	scanf("%d", &num);
-	temp = head;
-	while( temp -> next != NULL) {
-		temp = temp -> next;
-		if( num == temp -> data){
-			printf("%d\n", i);
-			pos = i;
-			break;
-		}
-		i++;
-	}
-	temp = head;
-	if ( head == NULL ){
-		printf("list is empty\n");
-	} else {
-		while( i < pos + 1 ) {
-			temp = temp -> next;
-			i++;
-		}
-		temp1 = temp -> next;
-		temp -> next = temp1 -> next;
-		free( temp1 );
-	}
-	return head;
-}
-struct Node *delete_mid( struct Node *head, int *newsize)
-{
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	int i = 1, pos, num;
-	temp = head;
-	pos = (*newsize) / 2;
-	if ( head == NULL ){
-		printf("list is empty\n");
-	} else {
-		while( i < pos ) {
-			temp = temp -> next;
-			i++;
-		}
-		temp1 = temp -> next;
-		temp -> next = temp1 -> next;
-		free( temp1 );
-	}
-	return head;
-}
-struct Node *delete_penultimate( struct Node *head, int *newsize)
-{
-	struct Node *temp = NULL;
-	struct Node *temp1 = NULL;
-	int i = 1, pos, num;
-	temp = head;
-	pos = *newsize - 1;
-	if ( head == NULL ){
-		printf("list is empty\n");
-	} else {
-		while( i < pos - 1 ) {
-			temp = temp -> next;
-			i++;
-		}
-		temp1 = temp -> next;
-		temp -> next = temp1 -> next;
-		free( temp1 );
-	}
-	return head;
+/* ---------- Create ---------- */
+struct Node *create_ll(int size) {
+    struct Node *head = NULL, *tail = NULL;
+
+    for (int i = 0; i < size; i++) {
+        struct Node *new_node = malloc(sizeof(struct Node));
+        printf("Enter data: ");
+        scanf("%d", &new_node->data);
+        new_node->next = NULL;
+
+        if (!head)
+            head = tail = new_node;
+        else {
+            tail->next = new_node;
+            tail = new_node;
+        }
+    }
+    return head;
 }
 
-
-
-int main(void)
-{
-	int size, choice;
-	struct Node *head = NULL;
-	struct Node *temp = NULL;
-	struct Node *new_node = NULL;
-	printf("enter size:\n");
-	scanf("%d",&size);
-	head = create_ll( head, temp, size);
-	while(1){
-		printf("1. Delete at the beginning\n2. Delete at the end\n3. Delete at a given position\n4. Delete before a given position\n5. Delete after a given position\n6. Delete before a given number\n7. Delete after a given number\n8. Delete at the middle\n9. Delete at penultimate node\n10. Print Linked list\n");
-		printf("enter ur choice:\n");
-		scanf("%d",&choice);
-		switch(choice){
-			case 1:
-				head = delete_begning( head );
-				break;
-			case 2:
-				head = delete_end( head );
-				break;
-			case 3:
-				head = delete_position( head );
-				break;
-			case 4:
-				head = delete_before_position( head );
-				break;
-			case 5:
-				head = delete_after_position( head );
-				break;
-			case 6:
-				head = delete_before_number( head );
-				break;
-			case 7:
-				head = delete_after_number( head );
-				break;
-			case 8:
-				head = delete_mid( head, &size );
-				break;
-			case 9:
-				head = delete_penultimate(head, &size);
-				break;
-			case 10:
-				print_ll( head );
-				exit(EXIT_SUCCESS);
-			default :
-				printf("wrong choice\n");
-//				exit(EXIT_SUCCESS);
-		}
-	}
+/* ---------- Delete Beginning ---------- */
+struct Node *delete_beginning(struct Node *head, int *size) {
+    if (!head) {
+        printf("List empty\n");
+        return head;
+    }
+    struct Node *temp = head;
+    head = head->next;
+    free(temp);
+    (*size)--;
+    return head;
 }
 
+/* ---------- Delete End ---------- */
+struct Node *delete_end(struct Node *head, int *size) {
+    if (!head) {
+        printf("List empty\n");
+        return head;
+    }
+    if (!head->next) {
+        free(head);
+        (*size)--;
+        return NULL;
+    }
+    struct Node *temp = head;
+    while (temp->next->next)
+        temp = temp->next;
 
+    free(temp->next);
+    temp->next = NULL;
+    (*size)--;
+    return head;
+}
 
+/* ---------- Delete At Position ---------- */
+struct Node *delete_at_position(struct Node *head, int *size) {
+    int pos;
+    printf("Enter position: ");
+    scanf("%d", &pos);
 
+    if (pos < 1 || pos > *size) {
+        printf("Invalid position\n");
+        return head;
+    }
+    if (pos == 1)
+        return delete_beginning(head, size);
 
+    struct Node *temp = head;
+    for (int i = 1; i < pos - 1; i++)
+        temp = temp->next;
 
+    struct Node *del = temp->next;
+    temp->next = del->next;
+    free(del);
+    (*size)--;
+    return head;
+}
 
+/* ---------- Delete Before Number ---------- */
+struct Node *delete_before_number(struct Node *head, int *size) {
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
 
+    if (!head || !head->next) {
+        printf("Not possible\n");
+        return head;
+    }
+    if (head->next->data == num)
+        return delete_beginning(head, size);
 
+    struct Node *prev = head, *curr = head->next;
+    while (curr->next && curr->next->data != num) {
+        prev = curr;
+        curr = curr->next;
+    }
+    if (!curr->next) {
+        printf("Number not found\n");
+        return head;
+    }
+    prev->next = curr->next;
+    free(curr);
+    (*size)--;
+    return head;
+}
 
+/* ---------- Delete After Number ---------- */
+struct Node *delete_after_number(struct Node *head, int *size) {
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
 
+    struct Node *temp = head;
+    while (temp && temp->data != num)
+        temp = temp->next;
 
+    if (!temp || !temp->next) {
+        printf("Deletion not possible\n");
+        return head;
+    }
+    struct Node *del = temp->next;
+    temp->next = del->next;
+    free(del);
+    (*size)--;
+    return head;
+}
 
+/* ---------- MAIN ---------- */
+int main() {
+    int size, choice;
+    struct Node *head = NULL;
 
+    printf("Enter initial size: ");
+    scanf("%d", &size);
+    head = create_ll(size);
 
+    while (1) {
+        printf("\n1.Delete Beginning\n2.Delete End\n3.Delete Position\n");
+        printf("4.Delete Before Number\n5.Delete After Number\n6.Print\n0.Exit\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
 
-
-
-
+        switch (choice) {
+            case 1: head = delete_beginning(head, &size); break;
+            case 2: head = delete_end(head, &size); break;
+            case 3: head = delete_at_position(head, &size); break;
+            case 4: head = delete_before_number(head, &size); break;
+            case 5: head = delete_after_number(head, &size); break;
+            case 6: print_ll(head); break;
+            case 0: exit(0);
+            default: printf("Invalid choice\n");
+        }
+    }
+}
